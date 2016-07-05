f7028fe2443c3fdffe73a0c7288e1324ed7dc4eb
Ted Stetzel <ted.stetzel@gmail.com>
2016-07-05 07:46:31 -0700

    Initial upload of scrape file

---
+++ scrape.py
@@ -0,0 +1,29 @@
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+
+
+pages = ['https://symphonycommerce.com/page1' 'https://symphonycommerce.com/page2']
+
+ids = ['800374','800108']
+
+i = 0 
+
+while i < len(pages):
+
+	url = pages[i]
+	id = ids [i]
+	response = requests.get(url)
+	html = response.content
+
+	soup = BeautifulSoup(html)
+	table = soup.find('div', attrs={'id': 'FeedingGuidelines'})
+	i = i + 1
+
+	# open a csv file with append, so old data will not be erased
+	with open('index_1.csv', 'a') as csv_file:  
+		writer = csv.writer(csv_file)
+		writer.writerow([url,table])
+	
+    
